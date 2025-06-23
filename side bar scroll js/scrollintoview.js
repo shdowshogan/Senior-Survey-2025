deb4c1109d8a8738f2a40c36c42f5aa8c05dd356
Ronit Gajbhiye <brucebanner0208@gmail.com>
2025-06-23 12:37:30 +0530

    made logo animation better

--- side bar scroll js/scrollintoview.js
+++ side bar scroll js/scrollintoview.js
@@ -1,9 +1,9 @@
 document.querySelectorAll('#sidebar a').forEach(link => {
-link.addEventListener('mouseenter', () => {
-const targetId = link.getAttribute('data-target');
-const target = document.getElementById(targetId);
-if (target) {
-    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-}
-});
+    link.addEventListener('mouseenter', () => {
+        const targetId = link.getAttribute('data-target');
+        const target = document.getElementById(targetId);
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
 });
