97106c4177ff8e2bd6eb70e1c27be16c6701bc9c
Mansi1975 <amruthammansi@gmail.com>
2025-06-25 18:44:07 +0530

    Save my local changes before pulling

--- script/index.js
+++ script/index.js
@@ -1,10 +1,24 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+// const menu = document.querySelector('#mobile-menu');
+// const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click',function(){
-    console.log('clicked')
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
+// menu.addEventListener('click',function(){
+//     console.log('clicked')
+//     menu.classList.toggle('is-active');
+//     menuLinks.classList.toggle('active');
+// });
 
 
+
+        function revealOnScroll() {
+            const sections = document.querySelectorAll('.section-container');
+            sections.forEach(section => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const windowHeight = window.innerHeight;
+                if (sectionTop < windowHeight - 100) {
+                    section.classList.add('visible');
+                }
+            });
+        }
+
+        window.addEventListener('scroll', revealOnScroll);
+        window.addEventListener('load', revealOnScroll);
