c7fabc83786affee79f12f832c498ab01eb26062
Ronit Gajbhiye <brucebanner0208@gmail.com>
2025-06-26 11:36:47 +0530

    enabled hamburger

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
-// const menu = document.querySelector('#mobile-menu');
-// const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-// menu.addEventListener('click',function(){
-//     console.log('clicked')
-//     menu.classList.toggle('is-active');
-//     menuLinks.classList.toggle('active');
-// });
+menu.addEventListener('click',function(){
+    console.log('clicked')
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
 
 
 
